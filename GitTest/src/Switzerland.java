e905526e2317ce08a2bc0891b08eeb7d3153e566
SMHRD <SMHRD@DESKTOP-S9PEDH0>
2023-04-14 16:52:15 +0900

    정다운 스위스 커밋

---
+++ GitTest/src/Switzerland.java
@@ -0,0 +1,32 @@
+
+public class Switzerland extends nation{
+	
+	
+	
+
+	@Override
+	public void Hello() {
+		System.out.println("Guete morge");
+		
+	}
+
+	@Override
+	public void population() {
+		System.out.println("150,000");
+		
+	}
+
+	@Override
+	public void capital() {
+		System.out.println("베른(Bern)");
+		
+	}
+
+	@Override
+	public String nationToKorean() {
+		return "스위스";
+	}
+	
+	
+
+}
