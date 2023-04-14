9281987d613baf896b6078046b582d546f75ce77
SMHRD <SMHRD@DESKTOP-5Q3732T>
2023-04-14 16:56:34 +0900

    Merge branch 'master' of https://github.com/2023-SMHRD-SW-Fullstack-1/middleFrontTeam.git

--- GitTest/src/Main.java
+++ GitTest/src/Main.java
@@ -5,8 +5,8 @@ public class Main {
 		
 		
 		// 각자의 이름에 맞게 객체 생성해주세요.
-		aaa a = new aaa(); // 정용
-		bbb b = new bbb(); // 소희
+		Thai ab = new Thai(); // 정용
+		China b = new China(); // 소희
 		ccc c = new ccc(); // 다운
 		ddd d = new ddd(); // 영석
 		
