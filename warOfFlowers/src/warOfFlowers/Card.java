9b0800739d4d95f6fea47b656e692e709068f1cf
orthh <witchicken2@gmail.com>
2023-04-17 17:07:28 +0900

    feat : make card

--- warOfFlowers/src/warOfFlowers/Card.java
+++ warOfFlowers/src/warOfFlowers/Card.java
@@ -2,4 +2,26 @@ package warOfFlowers;
 
 public class Card {
 	
+	private int number;
+	private String img;
+	
+	public Card() {
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public String getImg() {
+		return img;
+	}
+
+	public void setImg(String img) {
+		this.img = img;
+	}
+	
 }
