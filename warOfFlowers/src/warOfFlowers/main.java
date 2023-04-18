9760691fe6decdc7446e6c9339949e1d3f4b6cca
SMHRD <SMHRD@DESKTOP-S9PEDH0>
2023-04-18 15:23:31 +0900

    Merge branch 'master' of https://github.com/2023-SMHRD-SW-Fullstack-1/middleFrontTeam.git

---
+++ warOfFlowers/src/warOfFlowers/main.java
@@ -0,0 +1,52 @@
+package warOfFlowers;
+
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		JDBC jdbc = new JDBC();
+		
+		
+		int select;
+		while(true) {
+			System.out.print("[1]로그인 [2]회원가입 >> ");
+			select = sc.nextInt();
+			if(select==1) { //로그인
+				System.out.print("ID 입력 : ");
+				String id = sc.next();
+				System.out.print("PW 입력 : ");
+				String pw = sc.next();
+				System.out.println("NICK 입력 : ");
+				String nick = sc.next();
+
+				
+				System.out.print("[1]게임시작 [2]랭킹조회 [3]게임종료 >> ");
+				
+				
+				
+			}else if(select==2) {//회원가입
+				System.out.print("ID 입력 : ");
+				String id = sc.next();
+				System.out.print("PW 입력 : ");
+				String pw = sc.next();
+				System.out.println("NICK 입력 : ");
+				String nick = sc.next();
+				
+				
+				
+		
+			}else {
+				System.out.println("번호를 다시 입력해 주세요");
+			}
+		}
+		
+		
+		
+		
+		
+	}
+
+}
