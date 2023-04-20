d31e897687c5dae48a37131aa6593ae6abc27d1b
황정용 <jeongyong62@gmail.com>
2023-04-20 19:31:10 +0900

    main println 정리

--- warOfFlowers/src/warOfFlowers/main.java
+++ warOfFlowers/src/warOfFlowers/main.java
@@ -20,15 +20,9 @@ public class main {
 		int select;
 		boolean check = false;
 		int stop = -1;
-		int user = 0;
-		int user_1 = 0;
-		int user_score = 0;
-		int com1 = 0;
+		int user, user_1, user_score, com1, com1_score, com2, com2_score = 0;
 		int com1_1 = 0;
-		int com1_score = 0;
-		int com2 = 0;
 		int com2_1 = 0;
-		int com2_score = 0;
 		
 		
 		String a ="\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
@@ -52,27 +46,23 @@ public class main {
 		Label : while (stop == 0) {
 
 			if (check == false) {
-
 				sound.playStart();
+				
 				for (int i = 0; i < 6; i++) {
 
 					asc.sign();
 					Thread.sleep(5 * 50); // 일정기간동안 진행을 멈추어 반복 -> 반짝이는 효과
-					// asc.clearScreen();
 					asc.drawSign();
-					// asc.clearScreen();
 					Thread.sleep(5 * 50);
-					// asc.clearScreen();
 					asc.drawNewSign();
-					// asc.clearScreen();
-
 					if (i == 5) {
 						asc.drawNewSign2();
 					}
+					
 				}
 
-				System.out.println();
-				System.out.println(asc.rabbit());
+
+				System.out.println("\n" +asc.rabbit());
 				System.out.printf("%50s", "[1]로그인    [2]회원가입 ");
 				StringBuilder sb = new StringBuilder();
 				for (int i = 0; i < 31; i++) {
@@ -85,21 +75,18 @@ public class main {
 					String id = sc.next();
 					System.out.printf("%40s", "PW 입력 : ");
 					String pw = sc.next();
-					System.out.println("\n");
-
+					
 					player = dao.login(id, pw);
 					if (player != null) {
 						sound.playLogin();
-						System.out.printf("%55s", player.getNickname() + "님이 로그인 되었습니다." + "\n");
-						System.out.printf("%55s", player.getNickname() + "님 현재 포인트 : " + player.getPoint() + "\n");
-						System.out.println("\n");
+						System.out.printf("\n%55s", player.getNickname() + "님이 로그인 되었습니다." + "\n");
+						System.out.printf("%55s", player.getNickname() + "님 현재 포인트 : " + player.getPoint() + "\n\n\n");
 						check = true;
 						frame = new FrameController(player);
 						frame.makePlayer();
 						continue;
 					} else {
-						System.out.println();
-						System.out.println("로그인 실패! 다시 입력 해주세요");
+						System.out.println("\n로그인 실패! 다시 입력 해주세요");
 						Thread.sleep(40 * 50);
 					}
 
@@ -112,28 +99,24 @@ public class main {
 					String joinNick = sc.next();
 
 					if (joinNick.length() > 7 || joinId.length() > 7 || joinNick.length() < 3 || joinId.length() < 3) {
-						System.out.println();
-						System.out.printf("%55s", "ID와 NICK은 3글자 이상 최대 7글자까지 가능합니다.\n");
+						System.out.printf("\n%55s", "ID와 NICK은 3글자 이상 최대 7글자까지 가능합니다.\n");
 						Thread.sleep(40 * 50);
 						continue;
 					}
 
 					Boolean join = dao.signup(joinId, joinPw, joinNick);
 					if (join == true) {
-						System.out.println();
 						sound.playLogin();
-						System.out.printf("%50s", "회원가입 완료!\n");
+						System.out.printf("\n%50s", "회원가입 완료!\n");
 						Thread.sleep(30 * 50);
 
 					} else {
-						System.out.println();
-						System.out.printf("%55s", "중복된 아이디 입니다.\n");
+						System.out.printf("\n%55s", "중복된 아이디 입니다.\n");
 						Thread.sleep(40 * 50);
 					}
 
 				} else {
-					System.out.println();
-					System.out.printf("%55s", "번호를 다시 입력해 주세요\n");
+					System.out.printf("\n%55s", "번호를 다시 입력해 주세요\n");
 					Thread.sleep(40 * 50);
 				}
 			}
@@ -144,7 +127,6 @@ public class main {
 							"------------------------------------------------------------------------------------------------------\n\n");
 					System.out.printf("%55s", "[1]게임시작 [2]랭킹조회 [3]게임종료 ");
 					int mode = sc.nextInt();
-					System.out.println("\n");
 
 					frame.deleteFlower();
 					frame.changeComDieImage(false, 1);
@@ -159,9 +141,9 @@ public class main {
 						die.setC2(die.die(com2, user, com1));
 
 						while(true) {
-							System.out.printf("%55s", "[1]콜   [2]다이  [3]족보보기 ");
+							System.out.printf("\n%55s", "[1]콜   [2]다이  [3]족보보기 ");
 							select = sc.nextInt();
-							System.out.println();
+							
 							if (select == 1) {// 콜
 								sound.playDealing();
 
@@ -171,43 +153,38 @@ public class main {
 									frame.makeBlankFlower();
 									frame.changeComDieImage(true, 1);//
 									frame.changeComDieImage(true, 2);//
-
-									System.out.printf("%50s", "컴퓨터 1, 컴퓨터 2 다이");
-									System.out.println();
+									System.out.printf("\n%50s", "컴퓨터 1, 컴퓨터 2 다이\n");
 									continue;
+									
 								} else if (die.getC2()) {// 컴2이 죽었을때
 									user_1 = frame.makeFlower();
 									com1_1 = frame.makeFakeFlower();
 									frame.makeBlankFlower();
 									frame.changeComDieImage(true, 2);//
-
 									System.out.printf("%50s", "컴퓨터 2 다이");
-									System.out.println();
-									System.out.printf("%52s", "[1]오픈 [2]하프 ");
+									
 								} else if (die.getC1()) {// 컴1 죽었을때
 									user_1 = frame.makeFlower();
 									frame.makeBlankFlower();
 									com2_1 = frame.makeFakeFlower();
 									frame.changeComDieImage(true, 1); //
-
 									System.out.printf("%50s", "컴퓨터 1 다이");
-									System.out.println();
-									System.out.printf("%52s", "[1]오픈 [2]하프 ");
+									
 								}
 
 								else {// 아무도 안죽었을때
 									user_1 = frame.makeFlower();
 									com1_1 = frame.makeFakeFlower();
 									com2_1 = frame.makeFakeFlower();
-
-									System.out.println();
-									System.out.printf("%50s", "[1]오픈 [2]하프 ");
+									
 								}
-
+								System.out.printf("\n%52s", "[1]오픈 [2]하프 ");
 								select = sc.nextInt();
 								System.out.printf("------------------------------------------------------------------------------------------------------\n\n");
+								
 								if (select == 1) {// 오픈
 									sound.playDealing();
+									
 									if (die.getC1() && die.getC2() == false) {// 컴1이 죽었을때
 										frame.openFlower(1);
 										user_score = winLose.score(user, user_1);
@@ -235,9 +212,8 @@ public class main {
 										System.out.printf("%50s",
 												player.getNickname() + "님의 패 : " + winLose.scoreLevel(user_score) + "\n");
 										System.out.printf("%50s", "컴퓨터1의 패 : " + winLose.scoreLevel(com1_score) + "\n");
-									}
-
-									else {// 아무도 안죽었을때
+										
+									} else {// 아무도 안죽었을때
 										frame.openFlower();
 										user_score = winLose.score(user, user_1);
 										com1_score = winLose.score(com1, com1_1);
@@ -247,34 +223,29 @@ public class main {
 												player.getNickname() + "님의 패 : " + winLose.scoreLevel(user_score) + "\n");
 										System.out.printf("%50s", "컴퓨터1의 패 : " + winLose.scoreLevel(com1_score) + "\n");
 										System.out.printf("%50s", "컴퓨터2의 패 : " + winLose.scoreLevel(com2_score) + "\n");
+										
 									}
 
 									if (winLose.result(user_score, com1_score, com2_score) == 2) {
 										System.out.println(asc.win());
 										sound.playWin();
-
-										System.out.println();
-
 										player.setPoint(player.getPoint() + 200);
 										dao.setPoint(player);
-										System.out.printf("%55s", "+200 포인트 획득!\n");
+										System.out.printf("\n%55s", "+200 포인트 획득!\n");
 										System.out.printf("%55s",
 												player.getNickname() + "님 현재 포인트 : " + player.getPoint() + "\n");
+										
 									} else if (winLose.result(user_score, com1_score, com2_score) == 0) {
-										System.out.println();
-										System.out.println(asc.lose());
+										System.out.println("\n" + asc.lose());
 										sound.playLose();
-
-										System.out.println();
 										player.setPoint(player.getPoint() - 200);
 										dao.setPoint(player);
-										System.out.printf("%55s", "-200포인트 감점\n");
+										System.out.printf("\n%55s", "-200포인트 감점\n");
 										System.out.printf("%55s",
 												player.getNickname() + "님 현재 포인트 : " + player.getPoint() + "\n");
 
 									} else {
-										System.out.println();
-										System.out.println(asc.draw());
+										System.out.println("\n" + asc.draw());
 
 									}
 
@@ -282,9 +253,7 @@ public class main {
 									break;
 
 								} else if (select == 2) {// 하프
-									System.out.printf("%55s", "배팅포인트가 2배로 올라갑니다.\n");
-									System.out.println();
-
+									System.out.printf("%55s", "배팅포인트가 2배로 올라갑니다.\n\n");
 									frame.openFlower();
 									user_score = winLose.score(user, user_1);
 									com1_score = winLose.score(com1, com1_1);
@@ -295,32 +264,25 @@ public class main {
 									System.out.printf("%55s", "컴퓨터2의 패 : " + winLose.scoreLevel(com2_score) + "\n");
 
 									if (winLose.result(user_score, com1_score, com2_score) == 2) {
-										System.out.println();
-										System.out.println(asc.win());
+										System.out.println("\n" + asc.win());
 										sound.playWin();
-
-										System.out.println();
-
 										player.setPoint(player.getPoint() + 400);
 										dao.setPoint(player);
 										System.out.printf("%55s", "+400 포인트 획득!\n");
 										System.out.printf("%55s",
 												player.getNickname() + "님 현재 포인트 : " + player.getPoint() + "\n");
+										
 									} else if (winLose.result(user_score, com1_score, com2_score) == 0) {
-										System.out.println();
-										System.out.println(asc.lose());
+										System.out.println("\n" + asc.lose());
 										sound.playLose();
-
-										System.out.println();
 										player.setPoint(player.getPoint() - 400);
 										dao.setPoint(player);
-										System.out.printf("%55s", "-400포인트 감점\n");
+										System.out.printf("\n%55s", "-400포인트 감점\n");
 										System.out.printf("%55s",
 												player.getNickname() + "님 현재 포인트 : " + player.getPoint() + "\n");
 
 									} else {
-										System.out.println();
-										System.out.println(asc.draw());
+										System.out.println("\n" + asc.draw());
 
 									}
 
@@ -356,8 +318,8 @@ public class main {
 							String id = playerList.get(i).getId();
 							String nick = playerList.get(i).getNickname();
 							int point = playerList.get(i).getPoint();
-							System.out.printf("%60s",i + 1 + ". " + "ID : " + id + "\t NICK : " + nick + "\t POINT : " + point + "\n");
-							System.out.println();
+							System.out.printf("%60s",i + 1 + ". " + "ID : " + id + "\t NICK : " + nick + "\t POINT : " + point + "\n\n");
+
 						}
 
 					} else if (mode == 3) {// 게임종료
@@ -377,7 +339,6 @@ public class main {
 			}
 
 		}
-		System.out.println("Aaa");
 
 	}
 
