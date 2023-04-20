cc2a26debc2dddb5c3942f8f050e74aa6424e3e2
orthh <witchicken2@gmail.com>
2023-04-20 10:04:03 +0900

    style : 시연시 편의성 위해 화면 우측정렬 및 프레임 크기 조절

--- warOfFlowers/src/warOfFlowers/Frame.java
+++ warOfFlowers/src/warOfFlowers/Frame.java
@@ -2,29 +2,36 @@ package warOfFlowers;
 
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Toolkit;
+
 import javax.swing.JFrame;
 
 public class Frame extends JFrame {
 
 	private Container c;
-//	ArrayList<Integer> arr = new ArrayList<>();
-
+	
 	public Frame() {
-		// 리사이즈 못하게 막기
 		setTitle("warOfFlowers");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		c = getContentPane();
+	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		c.setLayout(new FlowLayout(FlowLayout.LEFT, 50, 12));
-		c.add(new PeopleLabel());
-		c.add(new ComputerLabel());
-		c.add(new ComputerLabel());
-		
-		c.setBackground(new Color(77, 70, 24));
-		setSize(480, 480);
-		setResizable(false);
-		setVisible(true);
+	    c = getContentPane();
+
+	    c.setLayout(new FlowLayout(FlowLayout.LEFT, 50, 12));
+	    c.add(new PeopleLabel());
+	    c.add(new ComputerLabel());
+	    c.add(new ComputerLabel());
+
+	    c.setBackground(new Color(77, 70, 24));
+	    setSize(480, 1080);
+	    setResizable(false);
+
+	    setLocationRelativeTo(null); // 화면 중앙에 위치하도록 설정
+
+	    int x = Toolkit.getDefaultToolkit().getScreenSize().width - getWidth(); // x좌표 계산
+	    setLocation(x, 0); // 오른쪽 정렬
+	    setVisible(true);
 	}
 
 	public Container getC() {
