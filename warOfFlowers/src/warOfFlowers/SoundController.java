cf8d535a718e87f9dc3d1270f4bdcc7fc6fd3121
황정용 <jeongyong62@gmail.com>
2023-04-20 15:11:30 +0900

    feat : SoundController win, lose 추가

--- warOfFlowers/src/warOfFlowers/SoundController.java
+++ warOfFlowers/src/warOfFlowers/SoundController.java
@@ -13,6 +13,8 @@ public class SoundController {
 	public SoundController() {
 		soundMap.put("shuffle", new Sound("shuffle", "sound/shuffle.mp3"));
 		soundMap.put("dealing", new Sound("dealing", "sound/dealing.mp3"));
+		soundMap.put("win", new Sound("win", "sound/win.mp3"));
+		soundMap.put("lose", new Sound("lose", "sound/lose.mp3"));
 	}
 	
 	// 셔플링 사운드
@@ -29,6 +31,20 @@ public class SoundController {
 			mp3.stop();
 		}
 		mp3.play(soundMap.get("dealing").getSoundPath());
+	}
+	// 승리 사운드
+	public void playWin() {
+		if (mp3.isPlaying()) {
+			mp3.stop();
+		}
+		mp3.play(soundMap.get("win").getSoundPath());
+	}
+	// 패배 사운드
+	public void playLose() {
+		if (mp3.isPlaying()) {
+			mp3.stop();
+		}
+		mp3.play(soundMap.get("lose").getSoundPath());
 	}
 
 	// 정지
